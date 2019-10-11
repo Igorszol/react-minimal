c7379c5a58ea9591756f83b44c3c636fb8a638f3
unknown <szoluchai@student.mini.pw.edu.pl>
2019-10-11 22:25:00 +0200

    Production bundle analysis

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
 const MyApp = () => (
-  <h1>Minimal React szoluchai</h1>  
+  <div><h1>Minimal React szoluchai </h1>  
+  <p>Bundle size: 91 bytes, Load time of the bundle: 157 ms, Last commit SHA1: 6889562e668d196e94b21bc31da27fc2586252ed</p></div>
+  
 )
 
 export default MyApp
