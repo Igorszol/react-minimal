07c30326170d99825c6c6d8dae5bcb56a395954b
unknown <szoluchai@student.mini.pw.edu.pl>
2019-10-20 21:32:11 +0200

    Lab2Ex4

--- src/Lab2.js
+++ src/Lab2.js
@@ -5,10 +5,11 @@ class Lab2 extends React.Component {
     constructor(props) {
         super();
 
-        this.state = {a:0, b:0, i:0,ar:new Array(0)};
+        this.state = {a:0, b:0, i:0,ar:new Array(0),tmp:new Array(0)};
     
         this.handleChangea = this.handleChangea.bind(this);
         this.handleChangeb = this.handleChangeb.bind(this);
+        this.process=this.process.bind(this);
     }
 
     handleChangea(event) {
@@ -17,6 +18,7 @@ class Lab2 extends React.Component {
         this.state.a=event.target.value;
         if(parseInt(this.state.a)>0 && parseInt(this.state.a)<parseInt(this.state.b))  this.state.ar=this.generateArray2(this.state.b-this.state.a);
         else this.state.ar=new Array(0);
+
     }
 
     handleChangeb(event) {
@@ -25,20 +27,31 @@ class Lab2 extends React.Component {
         this.state.b=event.target.value;
         if(parseInt(this.state.a)>0 && parseInt(this.state.a)<parseInt(this.state.b)) this.state.ar=this.generateArray2(this.state.b-this.state.a);
         else this.state.ar=new Array(0);    
+     
     }
 
     generateArray2(n){
         return Array.from({length: n+1}, (v,k) => parseInt(this.state.a,10)+k); 
     }
+
+    process= (event) =>
+    {
+         event.preventDefault();
+         this.setState({tmp: this.state.ar.map(Math.sqrt)})
+
+    }
+
     render() {
-        console.time("render : "+this.state.i+" ");      
+        console.time("render : "+this.state.i+" ");  
         return (
             <div>
                 <input id="numa" type="number" min="0" onChange={this.handleChangea}></input>
                 <input id="numb" type="number" min="0" onChange={this.handleChangeb}></input>
 
                 <p>{this.state.ar.toString()}</p>
-            {console.timeEnd("render : "+this.state.i++ +" ")}
+                <button onClick={this.process}>Process array</button>
+                <p>{this.state.tmp.toString()}</p>
+                {console.timeEnd("render : "+this.state.i++ +" ")}
             </div>
         )
     }
